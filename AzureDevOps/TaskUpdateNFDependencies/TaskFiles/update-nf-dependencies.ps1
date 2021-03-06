01a7ee6d2311cd3db3ba56afa0dee1f260c7fb14
Robin Jones <robin@networkfusion.co.uk>
2021-03-06 13:59:53 +0000

    Use correct file for conflict action

--- AzureDevOps/TaskUpdateNFDependencies/TaskFiles/update-nf-dependencies.ps1
+++ AzureDevOps/TaskUpdateNFDependencies/TaskFiles/update-nf-dependencies.ps1
@@ -164,12 +164,12 @@ ForEach($library in $librariesToUpdate)
                     if ($env:Build_SourceBranchName -like '*release*' -or $env:Build_SourceBranchName -like '*master*')
                     {
                         # don't allow prerelease for release and master branches
-                        Update-Package $solutionFile[0].FullName -Id "$packageName" -ConfigFile NuGet.Config -FileConflictAction OverwriteAll
+                        Update-Package $solutionFile[0].FullName -Id "$packageName" -ConfigFile NuGet.Config -FileConflictAction Overwrite
                     }
                     else
                     {
                         # allow prerelease for all others
-                        Update-Package $solutionFile[0].FullName -Id "$packageName" -ConfigFile NuGet.Config -PreRelease -FileConflictAction OverwriteAll
+                        Update-Package $solutionFile[0].FullName -Id "$packageName" -ConfigFile NuGet.Config -PreRelease -FileConflictAction Overwrite
                     }
 
 
